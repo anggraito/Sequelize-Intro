2104817bf2c17520a1fb0791de2d2098adce485b
anggraito laras <email": null>
2017-07-13 21:07:01 +0700

    push dibawa pulang

--- routers/students.js
+++ routers/students.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var ruut = express.Router();
+var ruut = express.ruut();
 
 var dbModel = require('../models');
 
@@ -9,5 +9,44 @@ ruut.get('/', function(req, res){
     res.render('Students', {data_students: rows});
   });
 });
+
+// ruut.get('/add', function(req, res){
+//   res.render('studentAdd');
+// });
+//
+// //add new student req.body
+// ruut.post('/add', function(req, res){
+//   Model.Student.create({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan })
+//   .then( function(){
+//     res.redirect('/students');
+//   })
+// });
+//
+// //go to the student edit form
+// ruut.get('/edit/:id', function(req, res){
+//   Model.Student.findById(req.params.id)
+//   .then (function (rows){
+//     res.render('studentEdit', {data_student: rows});
+//   })
+//    });
+//
+// ruut.post('/edit/:id', function(req, res) {
+//   Model.Student.update({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan},
+//     {
+//       where: {id: req.params.id}
+//     }
+//   )
+//   .then( function(){
+//   res.redirect('/students');
+//   })
+// });
+//
+// //delete data from student
+// ruut.get('/delete/:id', function(req, res){
+//     Model.Student.destroy({where: {id : req.params.id}})
+//     .then( function(){
+//   res.redirect('/students');
+//   })
+// });
 
 module.exports= ruut ;
