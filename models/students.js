2503114b84c65f07eb61d03412e8a0d9ad194ac7
anggraito laras <email": null>
2017-07-14 12:24:02 +0700

    push but still error edit and show data

--- models/students.js
+++ models/students.js
@@ -4,6 +4,6 @@ module.exports = function(sequelize, DataTypes) {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     email: DataTypes.STRING
-  });
+  }) ;
   return Students;
 };
--- routers/students.js
+++ routers/students.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var ruut = express.ruut();
+var ruut = express.Router();
 
 var dbModel = require('../models');
 
@@ -10,43 +10,46 @@ ruut.get('/', function(req, res){
   });
 });
 
-// ruut.get('/add', function(req, res){
-//   res.render('studentAdd');
-// });
-//
-// //add new student req.body
-// ruut.post('/add', function(req, res){
-//   Model.Student.create({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan })
-//   .then( function(){
-//     res.redirect('/students');
-//   })
-// });
-//
-// //go to the student edit form
-// ruut.get('/edit/:id', function(req, res){
-//   Model.Student.findById(req.params.id)
-//   .then (function (rows){
-//     res.render('studentEdit', {data_student: rows});
-//   })
-//    });
-//
-// ruut.post('/edit/:id', function(req, res) {
-//   Model.Student.update({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan},
-//     {
-//       where: {id: req.params.id}
-//     }
-//   )
-//   .then( function(){
-//   res.redirect('/students');
-//   })
-// });
-//
-// //delete data from student
-// ruut.get('/delete/:id', function(req, res){
-//     Model.Student.destroy({where: {id : req.params.id}})
-//     .then( function(){
-//   res.redirect('/students');
-//   })
-// });
+ruut.get('/add', function(req, res){
+  res.render('studentsAdd');
+});
+
+//add student req.body
+ruut.post('/add', function(req, res){
+  dbModel.Students.create(req.body)
+  .then( function(){
+    res.redirect('/students');
+  })
+  .catch(err =>{
+    res.render('studentsAdd',{title :'Add Students Data', errorMsg: err.message})
+  });
+});
+
+//edit form
+ruut.get('/edit/:id', function(req, res){
+  dbModel.Students.findById(req.params.id)
+  .then (function (rows){
+    res.render('studentsEdit', {data_students: rows});
+  });
+});
+
+ruut.post('/edit/:id', function(req, res) {
+  dbModel.Students.update(req.body ,{where: {id: req.params.id}});
+  .then( function(){
+    res.redirect('/students');
+  });
+  .catch(err =>{
+    res.render('studentsEdit',{title :'Input Data Students Not Succes', errorMsg: err.message})
+  });
+});
+
+
+//delete
+ruut.get('/delete/:id', function(req, res){
+    dbModel.Students.destroy({where: {id : req.params.id}})
+    .then( function(){
+  res.redirect('/Students');
+  })
+});
 
 module.exports= ruut ;
